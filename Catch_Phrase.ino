4526b272f6bc58b88e2e98e9d87eaf13af744dea
NoahGeorge <44949700+NoahGeorge@users.noreply.github.com>
2021-02-01 17:33:18 -0600

    h

--- Catch_Phrase.ino
+++ Catch_Phrase.ino
@@ -20,17 +20,35 @@
 const int rs = 15, en = 2, d4 = 32, d5 = 33, d6 = 25, d7 = 26;//Screen Pins
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-//Scoring
-int team1Score = 0;
-int team2Score = 0;
-const int WINNING_SCORE = 7;
+//team symbols
+
+<<<<<<< HEAD
+=======
+byte team1[] = {
+  B11100,
+  B01000,
+  B01000,
+  B00000,
+  B00110,
+  B00010,
+  B00010,
+  B00111
+};
+>>>>>>> parent of 73d73c4... Added most of the code needed
+
+byte team2[] = {
+  B11100,
+  B01000,
+  B01000,
+  B00000,
+  B00110,
+  B00001,
+  B00010,
+  B00111
+};
 
 
-File root;
-File category;
-
-int currentWord = 0;
-
+<<<<<<< HEAD
 //Timer
 volatile int secondCounter;
 
@@ -63,6 +81,8 @@ byte team2[] = {
 };
 
 
+=======
+>>>>>>> parent of 73d73c4... Added most of the code needed
 void printToScreen(char toScreen[], int layer, int type){//This function writes the charactor array to the screen. Type = 0 standard; Type = 1 category(removes .txt and \;
 
 
@@ -97,6 +117,7 @@ void printToScreen(char toScreen[], int layer, int type){//This function writes 
   
   
 }
+<<<<<<< HEAD
 
 void printTeamSymbols(){
   lcd.setCursor(0,0);
@@ -278,53 +299,119 @@ void testFileIO(fs::FS &fs, const char * path){
 int totalSecondCounter;
 hw_timer_t *timer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+=======
+>>>>>>> parent of 73d73c4... Added most of the code needed
 
-//Buttons
-struct Button
-{
-    const uint8_t pin;
-    volatile bool pressed;
-};
-Button TEAM_1_BUTTON = {16, false}, TEAM_2_BUTTON = {17, false}, START_BUTTON = {22, false}, CATEGORY_BUTTON = {14, false}, NEXT_BUTTON = {27, false};
-
-//team symbols
-byte team1[] = {B11100, B01000, B01000, B00000, B00110, B00010, B00010, B00111};
-byte team2[] = {B11100, B01000, B01000, B00000, B00110, B00001, B00010, B00111};
-
-//Interrupt Service Routines which become activated when a button is pressed
-void IRAM_ATTR team1ButtonFunction()
-{
-    TEAM_1_BUTTON.pressed = true;
+void printTeamSymbols(){
+  lcd.setCursor(0,0);
+  lcd.write(byte(0));
+  lcd.setCursor(15,0);
+  lcd.write(byte(1));
 }
 
-void IRAM_ATTR team2ButtonFunction()
-{
-    TEAM_2_BUTTON.pressed = true;
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){//lists the directories (not really needed for our purposes)
+    Serial.printf("Listing directory: %s\n", dirname);
+
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("Failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println("Not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.name(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("  SIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
 }
 
-void IRAM_ATTR startButtonFunction()
-{
-    START_BUTTON.pressed = true;
+void createDir(fs::FS &fs, const char * path){//creates a directory (not really needed for our purposes)
+    Serial.printf("Creating Dir: %s\n", path);
+    if(fs.mkdir(path)){
+        Serial.println("Dir created");
+    } else {
+        Serial.println("mkdir failed");
+    }
 }
 
-void IRAM_ATTR categoryButtonFunction()
-{
-    CATEGORY_BUTTON.pressed = true;
+void removeDir(fs::FS &fs, const char * path){//we don't really need this
+    Serial.printf("Removing Dir: %s\n", path);
+    if(fs.rmdir(path)){
+        Serial.println("Dir removed");
+    } else {
+        Serial.println("rmdir failed");
+    }
 }
 
-void IRAM_ATTR nextButtonFunction()
-{
-    NEXT_BUTTON.pressed = true;
+char* readFile(fs::FS &fs, const char * path){
+    //Serial.printf("Reading file: %s\n", path);
+
+    char oneLine[16];
+    Serial.println("Opening File");
+    File file = fs.open(path);
+    Serial.println("File Opened");
+    
+    int count = 0;
+    while(file.available()){
+        Serial.println("Reading File");
+        oneLine[count] = file.read();
+        Serial.println(oneLine[count]);
+        count++;
+    }
+    //Serial.println(oneLine);
+    file.close();
+    Serial.println("File Closed");
+    return oneLine;
 }
 
-//Timer Interrupt Service Routine
-void IRAM_ATTR onTimer()
-{
-    portENTER_CRITICAL_ISR(&timerMux);
-    secondCounter++;
-    portEXIT_CRITICAL_ISR(&timerMux);
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+    file.close();
 }
 
+void appendFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Appending to file: %s\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("Message appended");
+    } else {
+        Serial.println("Append failed");
+    }
+    file.close();
+}
+
+<<<<<<< HEAD
 /*
 ╔═══╗╔═══╗╔════╗╔╗─╔╗╔═══╗     ╔═══╗╔╗─╔╗╔═╗─╔╗╔═══╗╔════╗╔══╗╔═══╗╔═╗─╔╗
 ║╔═╗║║╔══╝║╔╗╔╗║║║─║║║╔═╗║     ║╔══╝║║─║║║║╚╗║║║╔═╗║║╔╗╔╗║╚╣─╝║╔═╗║║║╚╗║║
@@ -388,92 +475,69 @@ void loop()
             CATEGORY_BUTTON.pressed = false;
         }
         delay(10);
+=======
+void renameFile(fs::FS &fs, const char * path1, const char * path2){
+    Serial.printf("Renaming file %s to %s\n", path1, path2);
+    if (fs.rename(path1, path2)) {
+        Serial.println("File renamed");
+    } else {
+        Serial.println("Rename failed");
     }
-    START_BUTTON.pressed = false;
-    Serial.printf("Starting with category: %s\n", category.name());
-
-    //Shuffle word list
-    int numWords = getNumWordsInFile(category);
-    Serial.printf("Number of words in file: %d\n", numWords);
-    String wordList[numWords];
-    fillRandomizedWordArrayFromFile(wordList, category, numWords);
-
-    //Gameplay loop
-    while (team1Score < WINNING_SCORE && team2Score < WINNING_SCORE)
-    {
-        timerAlarmEnable(timer);
-
-        displayNextWord(wordList, numWords);
-
-        //Timer is running, word to guess is displayed on screen
-        while (totalSecondCounter < ROUND_TIME_IN_SECONDS)
-        {
-            //Get the next word when next button is pressed
-            if (NEXT_BUTTON.pressed)
-            {
-                displayNextWord(wordList, numWords);
-                NEXT_BUTTON.pressed = false;
-            }
-
-            //Update the clock if a second has passed
-            if (secondCounter > 0)
-            {
-                updateClock();
-            }
-        }
-
-        //Stop the timer and reset the timer values
-        Serial.println("Timer Expired");
-        timerAlarmDisable(timer);
-        TEAM_1_BUTTON.pressed = false;
-        TEAM_2_BUTTON.pressed = false;
-        secondCounter = 0;
-        totalSecondCounter = 0;
-
-        //Between rounds, waiting for score update or the start of the next round
-        while (START_BUTTON.pressed == false)
-        {
-            if (TEAM_1_BUTTON.pressed)
-            {
-                team1Score++;
-                Serial.printf("Team 1 earned a point. The score is now %d-%d\n", team1Score, team2Score);
-                printTeamScores();
-                TEAM_1_BUTTON.pressed = false;
-            }
-
-            if (TEAM_2_BUTTON.pressed)
-            {
-                team2Score++;
-                Serial.printf("Team 2 earned a point. The score is now %d-%d\n", team1Score, team2Score);
-                printTeamScores();
-                TEAM_2_BUTTON.pressed = false;
-            }
-
-            if (team1Score >= WINNING_SCORE)
-            {
-                Serial.println("Team 1 WINS.");
-                break;
-            }
-
-            if (team2Score >= WINNING_SCORE)
-            {
-                Serial.println("Team 2 WINS.");
-                break;
-            }
-
-            delay(10);
-        }
-        START_BUTTON.pressed = false;
-    }
-    //Game over
-    delay(4000);
-    lcd.clear();
-    team1Score = 0;
-    team2Score = 0;
-    currentWord = 0;
-    Serial.println("Starting new game.");
 }
 
+void deleteFile(fs::FS &fs, const char * path){
+    Serial.printf("Deleting file: %s\n", path);
+    if(fs.remove(path)){
+        Serial.println("File deleted");
+    } else {
+        Serial.println("Delete failed");
+>>>>>>> parent of 73d73c4... Added most of the code needed
+    }
+}
+
+void testFileIO(fs::FS &fs, const char * path){
+    File file = fs.open(path);
+    static uint8_t buf[512];
+    size_t len = 0;
+    uint32_t start = millis();
+    uint32_t end = start;
+    if(file){
+        len = file.size();
+        size_t flen = len;
+        start = millis();
+        while(len){
+            size_t toRead = len;
+            if(toRead > 512){
+                toRead = 512;
+            }
+            file.read(buf, toRead);
+            len -= toRead;
+        }
+        end = millis() - start;
+        Serial.printf("%u bytes read for %u ms\n", flen, end);
+        file.close();
+    } else {
+        Serial.println("Failed to open file for reading");
+    }
+
+
+    file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+
+    size_t i;
+    start = millis();
+    for(i=0; i<2048; i++){
+        file.write(buf, 512);
+    }
+    end = millis() - start;
+    Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
+    file.close();
+}
+
+<<<<<<< HEAD
 /*
 ╔╗─╔╗╔═══╗╔╗───╔═══╗╔═══╗╔═══╗     ╔═══╗╔╗─╔╗╔═╗─╔╗╔═══╗╔════╗╔══╗╔═══╗╔═╗─╔╗╔═══╗
 ║║─║║║╔══╝║║───║╔═╗║║╔══╝║╔═╗║     ║╔══╝║║─║║║║╚╗║║║╔═╗║║╔╗╔╗║╚╣─╝║╔═╗║║║╚╗║║║╔═╗║
@@ -495,6 +559,8 @@ void initSDCard()
   }
   uint8_t cardType = SD.cardType();
 =======
+=======
+>>>>>>> parent of 73d73c4... Added most of the code needed
 void setup(){
     
     
@@ -553,6 +619,7 @@ void setup(){
     Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
 
     char wordDir[] = "/"; //Directory of the word lists
+<<<<<<< HEAD
 
   
     SD.begin();
@@ -699,6 +766,154 @@ void loop(){
         }
       }   
     }
+=======
+
+  
+    SD.begin();
+    File root = SD.open(wordDir);
+    File category = root.openNextFile();
+
+    if(! category){//end of the list
+      Serial.println("No lists"); 
+    }else{
+    
+    printToScreen(strdup(category.name()),1,1);
+
+    Serial.println(strdup(category.name()));
+    }
+    root.close();
+    
+    
+
+    
+}
+
+void loop(){
+
+    //State of the left button
+    bool currStateLeft;
+    bool lastStateLeft;
+
+   //State of the Right button
+    bool currStateRight;
+    bool lastStateRight;
+
+    //State of the category button
+    bool currStateCategory;
+    bool lastStateCategory;
+
+    //State of the button for next
+    bool currStateNext;
+    bool lastStateNext;
+
+    //State if the button for Go
+    bool currStateGo;
+    bool lastStateGo;
+
+    
+
+    //Directory of the word lists
+    const char wordDir[] = "/"; 
+
+    SD.begin();
+    File root = SD.open(wordDir);
+    File category = root.openNextFile();
+
+    if(! category){//end of the list
+      Serial.println("No lists");
+    }
+  
+    
+
+  //char clearDisplay[16] = "                ";
+
+  char displayCategories[] = "Category:";
+  char phrase[17];
+
+  
+
+  
+  printToScreen(displayCategories, 0, 0);
+  int selCategory = 0;
+  
+  while(true){
+
+    currStateLeft     = digitalRead(16);//buttons for controlling selections
+    currStateRight    = digitalRead(17);
+    currStateCategory = digitalRead(14);
+    currStateGo       = digitalRead(35);
+    
+
+
+    //This detects if the left or right buttons have been pressed to change the catagory
+    if(currStateLeft != lastStateLeft || currStateRight != lastStateRight){//I
+      if(currStateLeft == 1 || currStateRight == 1){
+        if(currStateRight == 1){//Only gooes in one direction
+          //TODO: Use Doubly linked lists to travel the list in both directions
+          category = root.openNextFile();
+
+
+          char systemVolume[] = "/System Volume";
+          boolean sysVolSame = true;
+          for(int i = 0; i < strlen(systemVolume); ++i){
+            if(systemVolume[i] != category.name()[i]){
+              sysVolSame = false;
+              
+              break;
+            }
+          }
+          if(sysVolSame){//skip system volume information
+            Serial.println("System Volume Skipped");
+            category = root.openNextFile();
+          }
+          
+          if(! category){//end of the list
+            root.rewindDirectory();//go to list start
+            category = root.openNextFile();
+          }
+
+          
+          
+          printToScreen(strdup(category.name()),1,1);
+          Serial.println(category.name());
+          
+          
+        }
+      }
+    }
+    //This detects if the catagory has been chosen and starts the game
+    if(currStateGo != lastStateGo){
+      if(currStateGo== true){
+        if(!category.available()){
+          Serial.println("No words in category");
+          return;
+        }
+
+        unsigned long startTime = millis();
+        while(millis() < startTime + 60000){
+          currStateNext = digitalRead(27);
+          if(currStateNext != lastStateNext){
+            if(currStateNext == true){
+              Serial.println("Next Word");
+        
+            
+          
+              int i = 1;
+              while(category.peek() != '\n' && category.peek() != -1 && i < 14){
+                phrase[i] = category.read();
+                i++;
+                Serial.println(i);
+              }
+              //printToScreen(phrase,0,1);
+            }
+        
+          }
+          delay(10);
+          lastStateNext = currStateNext;
+        }
+      }   
+    }
+>>>>>>> parent of 73d73c4... Added most of the code needed
   
     delay(10);
     lastStateCategory = currStateCategory;
